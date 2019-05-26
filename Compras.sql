49a08c03f1c2145499c514bf8ea2a26ec500de6a
MuriloCunha <mcscunha@yahoo.com.br>
2019-05-26 18:50:30 -0300

    Renomeado arquivos RECEBMENSAL e DESPMENSAL

--- Compras.sql
+++
@@ -1,9 +0,0 @@
--- Nao validado
-select 
-    recnum, codfilial, dtlanc, dtvenc, codconta, 
-    historico, numnota, duplic, valor, dtpagto, vpago,
-    fornecedor
-from pclanc
-where dtpagto between '01/05/2019' and '31/05/2019'
-order by codfilial, codconta, dtpagto
-;
--- DespesasMensais.sql
+++
@@ -1,9 +0,0 @@
--- Nao validado
-select 
-    codcli, codfilial, prest, duplic, valor, dtvenc, 
-    vpago, dtpag, dtemissao, operacao, dtdesc, obs,
-    numtrans 
-from pcprest
-where dtpag between  '01/05/2019' and '31/05/2019'
-order by codfilial, dtpag
-;
