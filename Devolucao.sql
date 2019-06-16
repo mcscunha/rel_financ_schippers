35e22f7d9199896994d8843b4c7dd75101a17c0d
MuriloCunha <mcscunha@yahoo.com.br>
2019-06-16 17:54:53 -0300

    Correcao em formatacao

--- Devolucao.sql
+++ Devolucao.sql
@@ -1,43 +1,44 @@
-select 
-    'RESUMO_FATURA_AVULSA',
-    codfilial,
-    dtent,
-    codfornec,
-    null fornecedor,
-    numnota,
-    cliente,
-    vlfrete,
-    vldevolucao,
-    nome,
-    descricao,
-    motivo,
-    motivo2,
-    null observacao
-from 
-    VIEW_DEVOL_RESUMO_FATURAVULSA
+select
+  'RESUMO_FATURA_AVULSA',
+  codfilial,
+  dtent,
+  codfornec,
+  null fornecedor,
+  numnota,
+  cliente,
+  vlfrete,
+  vldevolucao,
+  nome,
+  descricao,
+  motivo,
+  motivo2,
+  null observacao
+from
+  VIEW_DEVOL_RESUMO_FATURAVULSA
 where
-    codfilial = '1'
-    and dtent between '01/03/2019' and '31/03/2019'
+  codfilial = '1'
+  and dtent between '01/03/2019'
+  and '31/03/2019'
 union all
-select 
-    'RESUMO_FATURAMENTO',
-    codfilial,
-    dtent,
-    codfornec,
-    fornecedor,
-    numnota,
-    cliente,
-    vlfrete,
-    vldevolucao,
-    nome,
-    descricao,
-    motivo,
-    motivo2,
-    obs
-from 
-    view_devol_resumo_faturamento
+select
+  'RESUMO_FATURAMENTO',
+  codfilial,
+  dtent,
+  codfornec,
+  fornecedor,
+  numnota,
+  cliente,
+  vlfrete,
+  vldevolucao,
+  nome,
+  descricao,
+  motivo,
+  motivo2,
+  obs
+from
+  view_devol_resumo_faturamento
 where
-    CONDVENDA NOT IN (4, 8, 10, 13, 20, 98, 99) 
-    and codfilial = '1'
-    and dtent between '01/03/2019' and '31/03/2019'
-;
+  CONDVENDA NOT IN (4, 8, 10, 13, 20, 98, 99)
+  and codfilial = '1'
+  and dtent between '01/03/2019'
+  and '31/03/2019';
