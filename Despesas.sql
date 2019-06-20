393e954e35734bd982ab7dce2b450e125c11b2be
MuriloCunha <mcscunha@yahoo.com.br>
2019-06-19 22:05:33 -0300

    Validacao em: Despesas, Devolucao, Bonificacao e Recebimentos

---
+++ Despesas.sql
@@ -0,0 +1,57 @@
+-- Despesas com Vendedores
+-- Validado em 19/06/2019
+-- Retirado com ajuda do Oracle SQLTracer
+--
+-- DESPESA MENSAL
+SELECT
+    a.codfilial,
+    c.tipo,
+    e.codigocentrocusto,
+    e.descricao,
+    c.codconta,
+    c.conta,
+    --a.historico,
+    c.grupoconta,
+    d.percrateio,
+    round((SUM(a.vpago) * (d.percrateio / 100)), 2) ValorTotal
+FROM
+    pclanc                     a
+    LEFT JOIN pcfornec         b ON b.codfornec = a.codfornec
+    JOIN pcconta               c ON c.codconta = a.codconta
+    JOIN pcrateiocentrocusto   d ON d.recnum = a.recnum
+    JOIN pccentrocusto         e ON e.codigocentrocusto = d.codigocentrocusto
+WHERE
+    ( nvl(c.investimento, 'N') <> 'S' )
+    AND c.grupoconta >= 200
+    AND c.grupoconta <= 900
+    AND nvl(a.codrotinabaixa, 0) NOT IN (737)
+    AND a.codfilial = '1'
+    AND (nvl(a.dtcompetencia, a.dtlanc) >= '01/03/2019')
+    AND (nvl(a.dtcompetencia, a.dtlanc) <= '31/03/2019')
+    AND d.codigocentrocusto in (
+            '1.10',     /* GLAUCO           */
+            '1.13',     /* BOVINOS SCHBR    */
+            '1.16',     /* CEZARIO          */
+            '1.19',     /* MARTIN           */
+            '1.22',     /* HUGO             */
+            '1.24',     /* ODIRLEI          */
+            '1.26',     /* VINICIUS         */
+            '1.3',      /* MARCOS           */
+            '1.34',     /* ALCENO           */
+            '1.35',     /* FELIPE           */
+            '1.6'       /* JOAO FERNANDES   */
+    )
+GROUP BY
+    a.codfilial,
+    c.tipo,
+    e.codigocentrocusto,
+    e.descricao,
+    c.codconta,
+    c.conta,
+    --a.historico,
+    d.percrateio,
+    c.grupoconta
+ORDER BY
+    descricao,
+    conta
+;
--- RecebMensal.sql
+++ RecebMensal.sql
@@ -1,12 +1,13 @@
 -- Valores validados
 -- Falta descobrir o que éa coluna CUSTO RECEBIMENTO
+
+-- RECEBIMENTO
 select
-  --*
   a.codfilial,
   a.codcli,
   b.estcob || ' - ' || b.cliente CLIENTE,
   a.codusur,
-  b.estcob || ' - ' || d.nome,
+  b.estcob || ' - ' || d.nome vendedor,
   a.duplic,
   a.prest,
   a.dtvenc,
@@ -22,15 +23,28 @@ select
   a.numtrans
 from
   pcprest a
-  join pcclient b on b.codcli = a.codcli
-  join pccob c on c.codcob = a.codcob
-  join pcusuari d on d.codusur = a.codusur
+join pcclient b 
+    on b.codcli = a.codcli
+join pccob c on 
+    c.codcob = a.codcob
+join pcusuari d on 
+    d.codusur = a.codusur
+--join pccentrocusto e on 
+--    e.codigocentrocusto = d.codigocentrocusto
 where
   a.dtpag between '01/03/2019' and '31/03/2019'
-  and a.codcob not in ('BNF', 'CRED') --and a.duplic in (24595, 24597, 24600)
+  and a.codcob not in ('BNF', 'CRED', 'CANC')
   and a.codfilial = '1'
-  and a.codusur = 22
+  and d.codusur in (
+        34, 36, 13,
+        16,  9, 28,
+        35, 10, 22,
+        30,  3, 19,
+        24
+  )
+  --and e.codigocentrocusto = '1.10'
 order by
   a.codfilial,
+  vendedor,
   a.dtpag
 ;
